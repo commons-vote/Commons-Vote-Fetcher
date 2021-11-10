e65445131f7bdb53b28362628b3bf13d7bdc83d0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-11-10 19:07:49 +0100

    Implement date_of_first_upload() method

--- Fetcher.pm
+++ Fetcher.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Class::Utils qw(set_params);
+use DateTime::Format::Strptime;
+use MediaWiki::API;
 
 our $VERSION = 0.01;
 
@@ -13,10 +15,45 @@ sub new {
 	# Create object.
 	my $self = bless {}, $class;
 
+	# MediaWiki api.
+	$self->{'mw_api'} = 'https://commons.wikimedia.org/w/api.php';
+
 	# Process parameters.
 	set_params($self, @params);
 
+	$self->{'_mw'} = MediaWiki::API->new;
+	$self->{'_mw'}->{'config'}->{'api_url'} = $self->{'mw_api'};
+
+	$self->{'_dt_parser'} = DateTime::Format::Strptime->new(
+		pattern => '%FT%T',
+		time_zone => 'UTC',
+	);
+
 	return $self;
+}
+
+sub date_of_first_upload {
+	my ($self, $user) = @_;
+
+	# Query for first uploaded image.
+	my $ref = $self->{'_mw'}->api({
+		action => 'query',
+		list => 'allimages',
+		aisort => 'timestamp',
+		aiuser => $user,
+		ailimit => 1,
+	});
+
+	# No images.
+	if (! @{$ref->{'query'}->{'allimages'}}) {
+		return;
+	}
+
+	# Timestamp of first uploaded image.
+	my $mw_timestamp = $ref->{'query'}->{'allimages'}->[0]->{'timestamp'};
+
+	# Return DateTime object.
+	return $self->{'_dt_parser'}->parse_datetime($mw_timestamp);
 }
 
 1;
